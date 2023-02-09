94476a864d68cee734264ce998b538e1425ca376
Grenic_Mars <luckylucianoya@gmail.com>
2023-02-09 12:12:01 +0800

    make nvim-tree handle directory smoothly

--- config/nvim/lua/core/autocmd.lua
+++ config/nvim/lua/core/autocmd.lua
@@ -12,7 +12,28 @@ augroup remember_folds
 	autocmd BufWinEnter *.* silent! loadview
 augroup END]]
 
--- two space for html file
+-- Two space for html and yuck file
 vim.cmd[[autocmd FileType html setlocal expandtab shiftwidth=2 tabstop=2]]
+vim.cmd[[autocmd FileType yuck setlocal expandtab shiftwidth=2 tabstop=2]]
 
+-- Treat ejs as html
 vim.cmd[[au BufNewFile,BufRead *.ejs set filetype=html]]
+
+-- Open nvim-tree if is a direcotry, and cd into that
+local function open_nvim_tree(data)
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
--- config/nvim/lua/pluginConfig/nvimtree.lua
+++ config/nvim/lua/pluginConfig/nvimtree.lua
@@ -7,19 +7,16 @@ nvim_tree.setup({
 	-- Keeps the cursor on the first letter of the filename when moving in the tree.
 	hijack_cursor = true,
 
-	disable_netrw = false,
-	-- Will automatically open the tree when running setup if startup buffer is a directory, is empty or is unnamed. nvim-tree window will be focused.
-	open_on_setup = true,
+	disable_netrw = true,
 	sort_by = "name",
-	-- This can be used to remove the default mappings in the tree.
-	remove_keymaps = false,
 
 	-- UI rendering setup
 	renderer = {
 		indent_width = 2,
+		highlight_opened_files = "all",
 
 		-- Enable file highlight for git attributes using `NvimTreeGit*` highlight groups. This can be used with or without the icons.
-		highlight_git = true,
+		highlight_git = false,
 
 		-- Configuration options for tree indent markers.
 		indent_markers = {
@@ -45,12 +42,12 @@ nvim_tree.setup({
 				folder = true,
 				folder_arrow = false,
 				git = false,
-				modified = false,
+				modified = true,
 			},
 			glyphs = {
 				default = "",
 				symlink = "",
-				bookmark = "",
+				bookmark = "",
 				folder = {
 					arrow_closed = "",
 					arrow_open = "",
@@ -61,19 +58,36 @@ nvim_tree.setup({
 					symlink = "",
 					symlink_open = "",
 				},
-				-- These git symbols are kinda confused here
 				git = {
-					unstaged = "",
-					staged = "",
+					unstaged = "",
+					staged = "S",
 					unmerged = "",
 					renamed = "➜",
-					untracked = "",
-					deleted = "",
-					ignored = "",
+					untracked = "U",
+					deleted = "",
+					ignored = "◌",
 				},
 			},
 		},
 		-- Whether to show the destination of the symlink.
-		symlink_destination = true,
+		symlink_destination = false,
+	},
+	update_focused_file = {
+		enable = true,
+		-- Update the root directory of the tree if the file is not under current root directory.
+		update_root = true,
+	},
+	-- Indicate which file have unsaved modification. (not worky?)
+	modified = {
+		enable = true,
+		-- Show modified indication on directory whose children are modified.
+		show_on_dirs = true,
+	},
+	actions = {
+		change_dir = {
+			-- Change the working directory when changing directories in the tree.
+			enable = true,
+			global = true,
+		},
 	},
 })
