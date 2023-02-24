6e2fa26ce0db16ffe6de8c9a754b434b58373b8f
Grenic_Mars <luckylucianoya@gmail.com>
2023-02-24 16:25:10 +0800

    add an eww layout widget

--- myScript/wayland/hyprland/layoutSwitcher.sh
+++ myScript/wayland/hyprland/layoutSwitcher.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-# A script to cycle through hyprland master and dwindle layout (currently only these two are available)
+# A script to cycle through hyprland master and dwindle layout (currently only these two are available) and update eww layout indirector
 
 # Usage: ./layoutSwitcher.sh
 
 current_layout=$(hyprctl getoption general:layout | grep "str" | cut -d " " -f 2 | tr -d '"')
 
-
 if [ "$current_layout" = "master" ]
 then
 	hyprctl keyword general:layout dwindle
+	eww update which_layout=dwindle
 else
 	hyprctl keyword general:layout master
+	eww update which_layout=master
 fi
