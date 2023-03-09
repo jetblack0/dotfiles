120f7129ca190158daf60016978eaefaa7ad62b5
Grenic_Mars <luckylucianoya@gmail.com>
2023-03-09 15:43:30 +0800

    fix issue on battery notication script

--- myScript/wayland/battery_notification.sh
+++ myScript/wayland/battery_notification.sh
@@ -21,14 +21,14 @@ do
 	then
 		dunstify -I ~/.config/dunst/images/icons/battery_low.svg "please charge your laptop !"
 		battery_low_count=1
+	fi
 
-		if [ "$capacity" -lt 5 ]
-		then
-			dunstify -u "crtical" -I ~/.config/dunst/images/icons/battery_empty.svg "No battery left, I will suspend to prevent losing your work after 20 seconds !"
-			sleep 20
-			[ "$(cat /sys/class/power_supply/ADP0/online)" = "1" ] && continue
-			systemctl suspend
-		fi
+	if [ "$capacity" -lt 5 ]
+	then
+		dunstify -u "crtical" -I ~/.config/dunst/images/icons/battery_empty.svg "No battery left, I will suspend to prevent losing your work after 20 seconds !"
+		sleep 20
+		[ "$(cat /sys/class/power_supply/ADP0/online)" = "1" ] && continue
+		systemctl suspend
 	fi
 	sleep $refresh_rate
 done
