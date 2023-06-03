3398ccfc3ea35fcc0e5374706aa0e6cc84f04355
Grenic_Mars <luckylucianoya@gmail.com>
2023-06-03 21:18:08 +0800

    nvim autocmd to hide nvimtree cursor

--- config/nvim/lua/core/autocmd.lua
+++ config/nvim/lua/core/autocmd.lua
@@ -39,6 +39,25 @@ end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
+-- Hide nvimtree cursor since we have better cursor line to use
+vim.api.nvim_create_autocmd({ 'WinEnter', 'BufWinEnter' }, {
+  pattern = 'NvimTree*',
+  callback = function()
+    local def = vim.api.nvim_get_hl_by_name('Cursor', true)
+    vim.api.nvim_set_hl(0, 'Cursor', vim.tbl_extend('force', def, { blend = 100 }))
+    vim.opt.guicursor:append('a:Cursor/lCursor')
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufLeave', 'WinClosed' }, {
+  pattern = 'NvimTree*',
+  callback = function()
+    local def = vim.api.nvim_get_hl_by_name('Cursor', true)
+    vim.api.nvim_set_hl(0, 'Cursor', vim.tbl_extend('force', def, { blend = 0 }))
+    vim.opt.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20'
+  end,
+})
+
 -- Change fcitx5 input method to english when press escape
 vim.cmd[[let fcitx5state=system("fcitx5-remote")]]
 vim.cmd[[autocmd InsertLeave * :silent let fcitx5state=system("fcitx5-remote")[0] | silent !fcitx5-remote -c]]
