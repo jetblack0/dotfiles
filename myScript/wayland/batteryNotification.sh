c5c8657e8b7b3892a19af490567f6ab3ac1115a5
Grenic_Mars <luckylucianoya@gmail.com>
2023-08-13 16:51:37 +0800

    use systemd to handle battery notification

--- myScript/wayland/batteryNotification.sh
+++ myScript/wayland/batteryNotification.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 # send notification if the battery is lower then 10, suspend this computer if the battery is lower than 5
 
-pgrep --full "home/jetblack/Development/myScript/wayland/batteryNotification.sh" >/dev/null || exit 0
-
 battery_low_count=0
 refresh_rate=60
+
 while true
 do
-	if [ -z "$XDG_CURRENT_DESKTOP" ]
-	then
-		exit 0
-	fi
-
 	capacity="$(cat /sys/class/power_supply/BAT1/capacity)"
 	status="$(cat /sys/class/power_supply/AC/online)"
 
@@ -24,13 +18,25 @@ do
 
 	if [ "$capacity" -lt 10 ] && [ $battery_low_count -eq 0 ]
 	then
-		dunstify -I ~/.config/dunst/images/icons/battery_low.svg "please charge your laptop !"
+		if [ -z "$(who | grep "wayland")" ]
+		then
+			wall "please charge your laptop!"
+		else
+			dunstify -I /home/jetblack/.config/dunst/images/icons/battery_low.svg "Battery notification" "please charge your laptop!"
+		fi
+
 		battery_low_count=1
 	fi
 
 	if [ "$capacity" -lt 5 ]
 	then
-		dunstify -u "crtical" -I ~/.config/dunst/images/icons/battery_empty.svg "No battery left, I will suspend to prevent losing your work after 20 seconds !"
+		if [ -z "$(who | grep "wayland")" ]
+		then
+			wall "No battery left, I will suspend to prevent losing your work after 20 seconds!"
+		else
+			dunstify -u "crtical" -I /home/jetblack/.config/dunst/images/icons/battery_empty.svg "Battery notification" "no battery left, I will suspend to prevent losing your work after 20 seconds!"
+		fi
+
 		sleep 20
 		[ "$(cat /sys/class/power_supply/ADP0/online)" = "1" ] && continue
 		systemctl suspend
