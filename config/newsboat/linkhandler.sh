6af7edab09ef155810a9ada23285a6506cace595
John <me@unazoomer.com>
2024-08-26 17:47:56 +0800

    Sync with legacy configurations
    Some configuration files for legacy applications, such as X11 programs,
    are out of sync. Fixed that.

--- config/newsboat/linkhandler.sh
+++ config/newsboat/linkhandler.sh
@@ -15,9 +15,8 @@ fi
 case "$url" in
 	*mkv|*webm|*mp4|*youtube.com/watch*|*youtube.com/playlist*|*youtu.be*)
 		setsid -f mpv --slang=en --fs --ytdl-raw-options=ignore-config=,sub-lang=en,write-auto-sub= -quiet "$url" >/dev/null 2>&1 ;;
-	*png|*jpg|*JPG|*jpe|*jpeg|*gif)
-		# TODO: change feh to nsxiv
-		curl -sL "$url" > "/tmp/$(echo "$url" | sed "s/.*\///;s/%20/ /g")" && feh "/tmp/$(echo "$url" | sed "s/.*\///;s/%20/ /g")"  >/dev/null 2>&1 & ;;
+	*png|*jpg|*JPG|*jpe|*jpeg|*gif|*preview.redd.it*)
+		curl -sL "$url" > "/tmp/$(echo "$url" | sed "s/.*\///;s/%20/ /g")" && nsxiv "/tmp/$(echo "$url" | sed "s/.*\///;s/%20/ /g")"  >/dev/null 2>&1 & ;;
 	*pdf|*cbz|*cbr)
 		curl -sL "$url" > "/tmp/$(echo "$url" | sed "s/.*\///;s/%20/ /g")" && zathura "/tmp/$(echo "$url" | sed "s/.*\///;s/%20/ /g")"  >/dev/null 2>&1 & ;;
 	*mp3|*flac|*opus|*mp3?source*)
