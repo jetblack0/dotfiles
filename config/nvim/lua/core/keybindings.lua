f3ca5fb42587bf2f72d933a6363575ee7561e926
John <me@unazoomer.com>
2024-08-28 21:39:58 +0800

    Add neovim keybindings for tmux navigation plugin

--- config/nvim/lua/core/keybindings.lua
+++ config/nvim/lua/core/keybindings.lua
@@ -8,7 +8,7 @@ local keymap = vim.api.nvim_set_keymap
 
 -- Normal Mode ----------------------------------------
 -- Spell check
-keymap("n", "<F6>", ":set spell!<CR>", opts)
+keymap("n", "<leader>s", ":set spell!<CR>", opts)
 -- Toggle nvimtree
 keymap("n", "<c-b>", ":NvimTreeToggle<CR>", opts)
 -- Get rid of search highlight
@@ -17,28 +17,41 @@ keymap("n", "<esc><esc>", ":noh<CR>:echo \"\"<CR>", opts)
 
 -- Manage window (awesome style)
 -- Resize split (try submode)
-keymap("n", "<c-h>", ":vertical resize -2<CR>", opts)
-keymap("n", "<c-l>", ":vertical resize +2<CR>", opts)
-keymap("n", "<c-Up>", ":resize +2<CR>", opts)
-keymap("n", "<c-Down>", ":resize -2<CR>", opts)
+keymap("n", "<a-h>", ":vertical resize -2<CR>", opts)
+keymap("n", "<a-l>", ":vertical resize +2<CR>", opts)
+keymap("n", "<a-k>", ":resize +2<CR>", opts)
+keymap("n", "<a-j>", ":resize -2<CR>", opts)
 -- Change the position of splits (can not map to ctrl shift?)
 keymap("n", "<c-a-l>", "<C-w><S-l>", opts)
 keymap("n", "<c-a-h>", "<C-w><S-h>", opts)
 keymap("n", "<c-a-k>", "<C-w><S-k>", opts)
 keymap("n", "<c-a-j>", "<C-w><S-j>", opts)
--- Switch split
-keymap("n", "<c-j>", "<c-w>w", opts)
-keymap("n", "<c-k>", "<c-w><c-w>", opts)
+-- Switch split focus (handled by vim-tmux-navigator plugin)
+-- keymap("n", "<c-j>", "<c-w>j", opts)
+-- keymap("n", "<c-k>", "<c-w>k", opts)
+-- keymap("n", "<c-h>", "<c-w>h", opts)
+-- keymap("n", "<c-l>", "<c-w>l", opts)
 -- full screen
 keymap("n", "<c-.>", "<c-w>_<c-w>|", opts)
 keymap("n", "<c-,>", "<c-w>=", opts)
 -- Change tag
-keymap("n", "<a-.>", "gt<CR>", opts)
-keymap("n", "<a-,>", "gT<CR>", opts)
+keymap("n", "<c-n>", "gt<CR>", opts)
+keymap("n", "<c-p>", "gT<CR>", opts)
+-- keymap("n", "<a-.>", "gt<CR>", opts)
+-- keymap("n", "<a-,>", "gT<CR>", opts)
 
 -- toggle markdown preview
 keymap("n", "<leader>m", ":MarkdownPreviewToggle<CR>", opts)
 -------------------------------------------------------
+
+
+-- Insert mode ----------------------------------------
+keymap("i", "<c-b>", "<esc>:NvimTreeToggle<CR>", opts)
+-- Switch split focus (by default vim-tmux-navigator doesn't bind in insert mode)
+keymap("i", "<c-l>", "<esc>:TmuxNavigateRight<CR>", opts)
+keymap("i", "<c-h>", "<esc>:TmuxNavigateLeft<CR>", opts)
+keymap("i", "<c-j>", "<esc>:TmuxNavigateDown<CR>", opts)
+keymap("i", "<c-k>", "<esc>:TmuxNavigateUp<CR>", opts)
 
 
 -- Virtual Mode ----------------------------------------
@@ -65,9 +78,13 @@ keymap("x", "<a-k>", ":move '<-2<CR>gv-gv", opts)
 keymap("n", "<leader>f", ":lua vim.lsp.buf.format({ async = true })<CR>", opts)
 keymap("n", "<leader>a", ":lua vim.lsp.buf.code_action()<CR>", opts)
 keymap("n", "<leader>q", ":lua vim.diagnostic.setloclist()<CR>", opts)
-keymap("n", "<a-j>", ":lua vim.diagnostic.goto_next({buffer=0})<CR>", opts)
-keymap("n", "<a-k>", ":lua vim.diagnostic.goto_prev({buffer=0})<CR>", opts)
-keymap("n", "<a-l>", ":lua vim.diagnostic.open_float()<CR>", opts)
+keymap("n", "<leader>j", ":lua vim.diagnostic.goto_next({buffer=0})<CR>", opts)
+keymap("n", "<leader>k", ":lua vim.diagnostic.goto_prev({buffer=0})<CR>", opts)
+keymap("n", "<leader>l", ":lua vim.diagnostic.open_float()<CR>", opts)
+
+-- Toggle cmp
+keymap("n", "<leader>z", ":lua require('cmp').setup.buffer { enabled = false }<CR>", opts)
+keymap("n", "<leader>x", ":lua require('cmp').setup.buffer { enabled = true }<CR>", opts)
 
 -- Live servers
 -- TODO: add a keybinding to toggle live-server if current directory has a index.html file
