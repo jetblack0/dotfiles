bb4f63dafb3810a543a09fad974d0a25ef34cafd
Henry Chi <me@unazoomer.com>
2025-04-18 13:29:55 +0800

    Add nsxiv keybinds and organize config structure
    - Add nsxiv key handler for selecting Hyprlock and favorite wallpapers
    - Organize Hyprland configuration files for better structure

--- script/programs/wayland/swww/utility.sh
+++ script/programs/wayland/swww/utility.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
 base_dir="$HOME/Resources/media/pictures/wallpaper/landscape"
-favorite="$base_dir"/nature/flowers.png
+favorite="$HOME/.config/hypr/subconfig/paths/favorite-wall"
+options="
+--transition-type grow
+--transition-fps 60
+"
 
 case "$1" in
 	random)
-		swww img "$(rg --files "$base_dir" | sort -R | tail -n 1)"
+		swww img $options "$(rg --files "$base_dir" | sort -R | tail -n 1)"
 		;;
 	favorite)
-		swww img "$favorite"
+		swww img $options "$favorite"
 		;;
 esac
 
