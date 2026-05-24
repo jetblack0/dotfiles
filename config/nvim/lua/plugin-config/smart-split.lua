7831591fd1d4fee9e11b9cd00eb7eb0616db9ad2
Henry Chi <me@unazoomer.com>
2026-05-24 14:07:36 +0800

    refactor(nvim): Enable some keybinds on t mode
    - mainly navigation keys

--- config/nvim/lua/plugin-config/smart-split.lua
+++ config/nvim/lua/plugin-config/smart-split.lua
@@ -2,14 +2,16 @@ local helpers = require("utils.helpers")
 local smart_splits = helpers.safe_require("smart-splits")
 
 smart_splits.setup({
-  disable_multiplexer_nav_when_zoomed = true,
-  multiplexer_integration = os.getenv("SMART_SPLITS_DISABLE") ~= "1",
+  disable_multiplexer_nav_when_zoomed = false,
+  -- nil = auto-detect (resolves to 'tmux' inside tmux); false = disabled.
+  -- Set SMART_SPLITS_DISABLE=1 to opt out (e.g. on ssh sessions).
+  multiplexer_integration = os.getenv("SMART_SPLITS_DISABLE") == "1" and false or nil,
 })
 
--- vim.keymap.set('n', '<A-h>', smart_splits.resize_left)
--- vim.keymap.set('n', '<A-j>', smart_splits.resize_down)
--- vim.keymap.set('n', '<A-k>', smart_splits.resize_up)
--- vim.keymap.set('n', '<A-l>', smart_splits.resize_right)
+vim.keymap.set({ 'n', 't' }, '<a-1>', smart_splits.resize_left)
+vim.keymap.set({ 'n', 't' }, '<a-2>', smart_splits.resize_down)
+vim.keymap.set({ 'n', 't' }, '<a-3>', smart_splits.resize_up)
+vim.keymap.set({ 'n', 't' }, '<a-4>', smart_splits.resize_right)
 -- moving between splits
 vim.keymap.set({ 'n', 't' }, '<C-h>', smart_splits.move_cursor_left)
 vim.keymap.set({ 'n', 't' }, '<C-j>', smart_splits.move_cursor_down)
