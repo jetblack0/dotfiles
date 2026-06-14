7d2c664aba2588141409fcde2134de57a18c9a4a
Henry Chi <me@unazoomer.com>
2026-06-14 18:21:12 +0800

    refactor(nvim): Refactor default codediff keybinds

--- config/nvim/lua/plugin-config/git/codediff.lua
+++ config/nvim/lua/plugin-config/git/codediff.lua
@@ -7,19 +7,46 @@ codediff.setup({
   keymaps = {
     view = {
       quit = "q",
-      toggle_explorer = "<leader>b",
+      toggle_explorer = "<c-b>",
       next_hunk = "<leader>gn",
       prev_hunk = "<leader>gN",
       show_help = "<leader>g?",
     },
     explorer = {
-      select = "<CR>",
+      select = "o",
+      toggle_staged = "s",
+      toggle_changes = "c",
       toggle_view_mode = "i",
       restore = "X",
     },
     history = {
-      select = "<CR>",
+      select = "o",
       toggle_view_mode = "i",
     },
+    conflict = {
+      next_conflict = "<leader>gn",
+      prev_conflict = "<leader>gN",
+      accept_incoming = "<leader>ct",
+      accept_current = "<leader>co",
+      accept_both = "<S-CR>",
+      discard = "<ESC>",
+      discard_all = "<S-ESC>",
+    }
   },
 })
+
+local default_map_opts = { noremap = true, silent = false }
+local function opt(desc, others)
+  return vim.tbl_extend("force", default_map_opts, { desc = desc }, others or {})
+end
+local keymap = vim.keymap.set
+
+keymap("n", "<leader>gd", ":CodeDiff<CR>", opt("Codediff on HEAD"))
+
+local default_map_opts = { noremap = true, silent = false }
+local function opt(desc, others)
+  return vim.tbl_extend("force", default_map_opts, { desc = desc }, others or {})
+end
+local keymap = vim.keymap.set
+
+keymap("n", "<leader>gd", ":CodeDiff<CR>", opt("Codediff on HEAD"))
