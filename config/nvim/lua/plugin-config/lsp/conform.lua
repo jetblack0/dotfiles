2509ae4ef98fd7023e3f4f6aeb157f6c4b3482f0
Henry Chi <me@unazoomer.com>
2026-07-25 15:45:20 +0800

    feat(nvim): Configure opentofu formatter

--- config/nvim/lua/plugin-config/lsp/conform.lua
+++ config/nvim/lua/plugin-config/lsp/conform.lua
@@ -14,11 +14,16 @@ conform.setup({
       'ruff_format',
     },
     -- groovy = { "npm-groovy-lint" },
+    -- Uses OpenTofu's `tofu fmt` (canonical HCL, same output as
+    -- `terraform fmt`). Doesn't rely on terraform-ls, which can't format
+    -- without the `terraform` binary installed.
+    terraform = { "tofu_fmt" },
+    ["terraform-vars"] = { "tofu_fmt" },
   },
 })
 
 -- Keybindings.
-vim.keymap.set("", "<leader>F", function() conform.format({ async = true, lsp_fallback = true }) end, {
+vim.keymap.set("", "<leader>F", function() conform.format({ async = true, lsp_format = "fallback" }) end, {
   desc = "Conform format",
   noremap = true,
   silent = false
