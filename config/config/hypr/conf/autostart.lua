15c49e92bece1485dced7821ee37f3b75aaf6ffe
Henry Chi <me@unazoomer.com>
2026-08-30 15:49:57 +0800

    fix(desktop): Set the cursor theme from hyprland

--- config/config/hypr/conf/autostart.lua
+++ config/config/hypr/conf/autostart.lua
@@ -5,4 +5,12 @@
 
 hl.on("hyprland.start", function()
 	hl.exec_cmd("noctalia")
+
+	-- cursor theme
+	hl.exec_cmd("hyprctl setcursor capitaine-cursors 24")
+
+	-- gtk settings
+	hl.exec_cmd("gsettings set org.gnome.desktop.interface icon-theme 'Tela-circle-black-dark'")
+	hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-theme 'capitaine-cursors'")
+	hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-size 24")
 end)
